ae717623ead0a0f83a559a9a00a522761af5c461
Ezabulbary <ezabul.bary@gmail.com>
2022-02-10 18:53:18 +0600

    decrementing loop in javascript

---
+++ decrementing-loop.js
@@ -0,0 +1,2 @@
+// decrementing - loop.js
+
--- factorial-function.js
+++ factorial-function.js
@@ -1,5 +1,6 @@
 // how to call factorial in function
 
+// for loop 
 function getFactorial(number){
     let factorial = 1;
     for(let i = 1; i <= number; i++){
@@ -10,4 +11,21 @@ function getFactorial(number){
 
 let myFactorial = 7;
 let factorialNumber = getFactorial(myFactorial);
-console.log('My Factorial NUmber is: ', factorialNumber);  
+console.log('My First Factorial NUmber is: ', factorialNumber);  
+
+// while loop 
+
+function getFactorial(number){
+    let factorial = 1;
+    let i = 1;
+    while (i < number){
+        factorial = factorial * i;
+        i++;
+    }
+    return factorial;
+}
+
+let myNumber = 9;
+let myFactorialNumber = getFactorial(myNumber);
+
+console.log('My Seconde Factorial NUmber is: ', myFactorialNumber);
