8407a5798508457f16d823c74ba368e725f9b431
Ezabulbary <ezabul.bary@gmail.com>
2022-02-10 13:10:32 +0600

    odd and even number added

--- inche-and-feet.js
+++ inche-and-feet.js
@@ -1,3 +1,5 @@
+// inche to feet
+
 function myInches(inche){
     var feet = inche / 12;
     return feet;
@@ -11,3 +13,14 @@ console.log('dadi inches to feet', feet);
 var dadaInche = 158;
 var feet = myInches(dadaInche);
 console.log('dadainche to feet', feet);
+
+
+// km to mile
+
+function mileToKilomiter(mile){
+    var km = mile * 1.60934;
+    return km;
+}
+
+var myKm = mileToKilomiter(48);
+console.log('This is my KM: ', myKm);
