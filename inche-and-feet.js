2178697acd8031fdf1aa53da18633eee07ec76e9
Ezabulbary <ezabul.bary@gmail.com>
2022-02-10 12:58:58 +0600

    inche to feet added

---
+++ inche-and-feet.js
@@ -0,0 +1,13 @@
+function myInches(inche){
+    var feet = inche / 12;
+    return feet;
+}
+
+var dadiInche = 120;
+var feet = myInches(dadiInche);
+console.log('dadi inches to feet', feet);
+
+
+var dadaInche = 158;
+var feet = myInches(dadaInche);
+console.log('dadainche to feet', feet);
